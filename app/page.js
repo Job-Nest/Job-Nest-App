152d4fa2448326c539ea32759702b77ada7f34ca
Vicky Hoang <vicky.k.hoang@gmail.com>
2024-02-13 11:26:38 -0800

    Define file structure Co-authored-by: Charmie Dubongco <charmiedubongco@gmail.com> Co-authored-by: Zoren Labrador <zorenzal@gmail.com> Co-authored-by: Kevin Chou <choukevin@berkeley.edu> Co-authored-by: Julian Bos <julianb1212@gmail.com>

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default async function Home() {
     POSTGRES_URL: process.env.POSTGRES_URL,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
   });
-  const { rows } = await sql`SELECT * from test;`;
+  const { rows } = await sql`SELECT * from users;`;
   console.log(rows);
   return (
     <main className={styles.main}>
