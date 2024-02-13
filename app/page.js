55a66f9f5099a72f5ae3ce07309b41e8be4a4046
Kevin Chou <choukevin@berkeley.edu>
2024-02-13 11:41:28 -0800

    test

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default async function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started by editing,&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
